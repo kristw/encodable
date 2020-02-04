806ef7da74098d09d8b3cb057171d2c4e3d1304f
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-02-04 01:32:05 -0800

    fix: datetime parsing

--- packages/encodable/src/parsers/parseDateTime.ts
+++ packages/encodable/src/parsers/parseDateTime.ts
@@ -1,5 +1,4 @@
-import { parse, codegen } from 'vega-expression';
-import { dateTimeExpr } from 'vega-lite/build/src/datetime';
+import { dateTimeToTimestamp } from 'vega-lite/build/src/datetime';
 import { DateTime } from '../types/VegaLite';
 
 export default function parseDateTime(dateTime: string | number | DateTime) {
@@ -7,25 +6,5 @@ export default function parseDateTime(dateTime: string | number | DateTime) {
     return new Date(dateTime);
   }
 
-  const expression = dateTimeExpr(dateTime, true) as string;
-  const code = codegen({ globalvar: 'window' })(parse(expression)).code as string;
-  // Technically the "code" here is safe to eval(),
-  // but we will use more conservative approach and manually parse at the moment.
-  const isUtc = code.startsWith('Date.UTC');
-
-  const dateParts = code
-    .replace(/^(Date\.UTC|new Date)\(/, '')
-    .replace(/\)$/, '')
-    .split(',')
-    .map((chunk: string) => Number(chunk.trim())) as [
-    number, // year
-    number, // month
-    number, // date
-    number, // hours
-    number, // minutes
-    number, // seconds
-    number, // milliseconds
-  ];
-
-  return isUtc ? new Date(Date.UTC(...dateParts)) : new Date(...dateParts);
+  return new Date(dateTimeToTimestamp(dateTime));
 }
