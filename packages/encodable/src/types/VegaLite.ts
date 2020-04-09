744e9d255a1e3bf4d80214fc89efea048eb6df37
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-09 14:14:07 -0700

    build: update vega-lite version

--- packages/encodable/src/types/VegaLite.ts
+++ packages/encodable/src/types/VegaLite.ts
@@ -1,11 +1,24 @@
 import { ValueOf } from './Base';
 
 // Types imported from vega-lite
-export { ValueDef, Value } from 'vega-lite/build/src/channeldef';
 export { isDateTime, DateTime } from 'vega-lite/build/src/datetime';
 export { SchemeParams, Scale, NiceTime } from 'vega-lite/build/src/scale';
 export { Axis } from 'vega-lite/build/src/axis';
 export { Type } from 'vega-lite/build/src/type';
+
+/** Same with vega-lite's Value */
+export type Value = number | string | boolean | null;
+
+/**
+ * Override vega-lite's ValueDef which also supports Gradient
+ * Definition object for a constant value (primitive value or gradient definition) of an encoding channel.
+ */
+export interface ValueDef<V extends Value | Value[] = Value> {
+  /**
+   * A constant value in visual domain (e.g., `"red"` / `"#0099ff"` / [gradient definition](https://vega.github.io/vega-lite/docs/types.html#gradient) for color, values between `0` to `1` for opacity).
+   */
+  value: V;
+}
 
 // Override this implementation
 // because vega-lite uses namespace which has issues with babel and typescript
