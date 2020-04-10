37d46c604a5800f0381e14f05cef1342757b07c1
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-10 12:19:42 -0700

    feat: export types typeguards and merge encoding utils (#17)
    * feat: export mergeEncoding
    
    * feat: export types and typeguards

--- packages/encodable/src/index.ts
+++ packages/encodable/src/index.ts
@@ -3,3 +3,23 @@ export { default as ChannelEncoder } from './encoders/ChannelEncoder';
 export { default as createEncoderFactory } from './encoders/createEncoderFactory';
 export { default as completeChannelDef } from './fillers/completeChannelDef';
 export { default as createScaleFromScaleConfig } from './parsers/scale/createScaleFromScaleConfig';
+export { default as mergeEncoding } from './utils/mergeEncoding';
+
+// Export type guards
+export {
+  isFieldDef,
+  isNonValueDef,
+  isTypedFieldDef,
+  isValueDef,
+  isScaleFieldDef,
+  isPositionFieldDef,
+} from './typeGuards/ChannelDef';
+
+export { isCompleteFieldDef, isCompleteValueDef } from './typeGuards/CompleteChannelDef';
+
+// Export types
+export * from './types/Data';
+export * from './types/Encoding';
+export * from './types/Legend';
+export * from './types/Scale';
+export * from './types/VegaLite';
