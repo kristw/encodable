3691200b94fefff7272e8b3f7fffd1554c0c7c51
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-10 12:15:30 -0700

    feat: export types and typeguards

--- packages/encodable/src/index.ts
+++ packages/encodable/src/index.ts
@@ -4,3 +4,22 @@ export { default as createEncoderFactory } from './encoders/createEncoderFactory
 export { default as completeChannelDef } from './fillers/completeChannelDef';
 export { default as createScaleFromScaleConfig } from './parsers/scale/createScaleFromScaleConfig';
 export { default as mergeEncoding } from './utils/mergeEncoding';
+
+// Export type guards
+export {
+  isFieldDef,
+  isNonValueDef,
+  isTypedFieldDef,
+  isValueDef,
+  isScaleFieldDef,
+  isPositionFieldDef,
+} from './typeGuards/ChannelDef';
+
+export { isCompleteFieldDef, isCompleteValueDef } from './typeGuards/CompleteChannelDef';
+
+// Export types
+export * from './types/Data';
+export * from './types/Encoding';
+export * from './types/Legend';
+export * from './types/Scale';
+export * from './types/VegaLite';
