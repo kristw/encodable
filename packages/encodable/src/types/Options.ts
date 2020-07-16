fe1a8c04cbee41a06d30de0e222d654bb685fce8
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-07-15 17:36:08 -0700

    fix: handle local time format

--- packages/encodable/src/types/Options.ts
+++ packages/encodable/src/types/Options.ts
@@ -5,7 +5,10 @@ export type NumberFormatter = (value: number | null | undefined) => string;
 export type NumberFormatResolver = (format?: string) => NumberFormatter;
 
 export type TimeFormatter = (value: Date | number | null | undefined) => string;
-export type TimeFormatResolver = (format?: string) => TimeFormatter;
+export type TimeFormatResolver = (params: {
+  format?: string;
+  formatInLocalTime?: boolean;
+}) => TimeFormatter;
 
 export type CategoricalColorScaleResolver = (params: {
   name?: string;
