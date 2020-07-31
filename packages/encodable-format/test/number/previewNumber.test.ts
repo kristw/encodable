c8c45f2232402dd896a40893db444cc295dd978e
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-07-31 16:32:43 -0700

    test: fix unit test

--- packages/encodable-format/test/number/previewNumber.test.ts
+++ packages/encodable-format/test/number/previewNumber.test.ts
@@ -6,4 +6,9 @@ describe('previewNumber(format, number)', () => {
       '200 => 200.00',
     );
   });
+  it('has default preview value', () => {
+    expect(previewNumber((v: number | null | undefined) => `${v?.toFixed(2)}`)).toEqual(
+      '12345.432 => 12345.43',
+    );
+  });
 });
