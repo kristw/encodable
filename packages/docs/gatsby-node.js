fb8df7b6b669a141aef91559efdca1d9c1064b06
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-08-22 01:11:08 -0700

    fix: eslint in doc

--- packages/docs/gatsby-node.js
+++ packages/docs/gatsby-node.js
@@ -1,18 +1,18 @@
-/* eslint-disable no-undef */
-module.exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
-  const config = getConfig();
+// /* eslint-disable no-undef */
+// module.exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+//   const config = getConfig();
 
-  const newRules = config.module.rules.filter(
-    rule =>
-      !(
-        rule.use &&
-        rule.use[0] &&
-        rule.use[0].options &&
-        rule.use[0].options.useEslintrc !== undefined
-      ),
-  );
+//   const newRules = config.module.rules.filter(
+//     rule =>
+//       !(
+//         rule.use &&
+//         rule.use[0] &&
+//         rule.use[0].options &&
+//         rule.use[0].options.useEslintrc !== undefined
+//       ),
+//   );
 
-  config.module.rules = newRules;
+//   config.module.rules = newRules;
 
-  actions.replaceWebpackConfig(config);
-};
+//   actions.replaceWebpackConfig(config);
+// };
