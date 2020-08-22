be8cea26cc177a3d75192c7a136807dfe06cd8ab
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-08-22 00:10:10 -0700

    docs: customize typography

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,4 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  plugins: ['gatsby-theme-docz'],
+  plugins: [
+    'gatsby-theme-docz',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto+Slab\:700`,
+          `Roboto\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
+    },
+  ],
 };
