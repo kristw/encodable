258c70ee9c7517e405beb30c11ba8321ce31d327
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-09-15 17:49:49 -0700

    docs: add bar example

--- packages/docs/src/components/SketchBarChart/index.tsx
+++ packages/docs/src/components/SketchBarChart/index.tsx
@@ -17,19 +17,24 @@ export interface BarChartProps {
   };
 }
 
-const padding = 30;
-
-export default function BarChart({ data = [], encoding, width, height }: BarChartProps) {
+export default function BarChart({
+  data = [],
+  padding = {},
+  encoding,
+  width,
+  height,
+}: BarChartProps) {
+  const { top = 30, left = 162, bottom = 30, right = 60 } = padding;
   // Parse encoding and create encoder
   const encoder = useMemo(() => barChartEncoderFactory.create(encoding), [encoding]);
 
   encoder.setDomainFromDataset(data);
   if (encoder.channels.x.scale) {
-    encoder.channels.x.scale.range([0, width - padding * 2]);
+    encoder.channels.x.scale.range([0, width - (left + right)]);
   }
   let band = 10;
   if (encoder.channels.y.scale) {
-    encoder.channels.y.scale.range([0, height - padding * 2]);
+    encoder.channels.y.scale.range([0, height - (top + bottom)]);
     if ('bandwidth' in encoder.channels.y.scale) {
       band = encoder.channels.y.scale.bandwidth();
     }
@@ -37,27 +42,47 @@ export default function BarChart({ data = [], encoding, width, height }: BarChar
 
   const { x, y, label, color } = encoder.channels;
 
-  try {
-    console.log('hi', y.encodeDatum(data[0], 1));
-  } catch (error) {
-    console.log('e', error);
-  }
-
-  return encoder.channels.y.scale ? y.encodeDatum(data[0], 1) : <>no</>;
-
   return (
-    <ReactRough width={width} height={height} renderer="svg">
-      {data.map((d, i) => (
-        <Rectangle
-          // eslint-disable-next-line react/no-array-index-key
-          key={i}
-          x={padding}
-          y={padding + y.encodeDatum(d, 0)}
-          width={x.encodeDatum(d, 0)}
-          height={band}
-          fill={color.encodeDatum(d, 'none')}
-        />
-      ))}
-    </ReactRough>
+    <div style={{ position: 'relative' }}>
+      <ReactRough width={width} height={height} renderer="svg">
+        {data.map(d => (
+          <Rectangle
+            key={label.formatDatum(d)}
+            x={left}
+            y={top + y.encodeDatum(d, 0)}
+            width={x.encodeDatum(d, 0)}
+            height={band}
+            fill={color.encodeDatum(d, 'none')}
+            stroke={color.encodeDatum(d, 'none')}
+          />
+        ))}
+      </ReactRough>
+      <svg style={{ position: 'absolute', top: 0, left: 0 }} width={width} height={height}>
+        {data.map(d => (
+          <>
+            <text
+              key={label.formatDatum(d)}
+              dy=".3em"
+              x={left - 10}
+              y={top + y.encodeDatum(d, 0) + band / 2}
+              fill={color.encodeDatum(d, 'none')}
+              textAnchor="end"
+            >
+              {label.formatDatum(d)}
+            </text>
+            <text
+              key={label.formatDatum(d)}
+              dy=".3em"
+              x={left + x.encodeDatum(d, 0) + 10}
+              y={top + y.encodeDatum(d, 0) + band / 2}
+              fill={color.encodeDatum(d, 'none')}
+              textAnchor="start"
+            >
+              {x.formatDatum(d)}
+            </text>
+          </>
+        ))}
+      </svg>
+    </div>
   );
 }
