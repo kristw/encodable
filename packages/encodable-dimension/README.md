e4569041143fc43716dad16378c7019fd077b37b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-09-18 17:25:48 -0700

    feat: add dimension package (#112)
    * feat: add dimension utilities
    
    * fix: type
    
    * fix: types

---
+++ packages/encodable-dimension/README.md
@@ -0,0 +1,16 @@
+## @encodable/dimension
+
+[![Version](https://img.shields.io/npm/v/@encodable/dimension.svg?style=flat)](https://img.shields.io/npm/v/@encodable/dimension.svg?style=flat)
+[![David (path)](https://img.shields.io/david/kristw/encodable.svg?path=packages%2Fencodable-dimension&style=flat-square)](https://david-dm.org/kristw/encodable?path=packages/encodable-dimension)
+
+Utilities for computing dimensions.
+
+### Install
+
+```sh
+npm install @encodable/dimension
+```
+
+### Documentation
+
+[See documentation and more examples here.](https://encodable.vercel.app/encodable-dimension)
---
+++ packages/encodable-dimension/src/mergeMargin.ts
@@ -0,0 +1,37 @@
+import { MarginOrPadding } from './types';
+
+function isNumber(x: number | null | undefined): x is number {
+  return x != null && !Number.isNaN(x);
+}
+
+function mergeSide(
+  operation: (a: number, b: number) => number,
+  a: number | null | undefined,
+  b: number | null | undefined,
+  defaultValue: number,
+): number {
+  if (isNumber(a)) {
+    return isNumber(b) ? operation(a, b) : a;
+  }
+  return isNumber(b) ? b : defaultValue;
+}
+
+function mergeMarginOrPadding(
+  margin1: Partial<MarginOrPadding> = {},
+  margin2: Partial<MarginOrPadding> = {},
+  mode: 'max' | 'min' = 'max',
+  defaultValue: number = 0,
+) {
+  const { top, left, bottom, right } = margin1;
+  const operation = mode === 'max' ? Math.max : Math.min;
+
+  return {
+    top: mergeSide(operation, top, margin2.top, defaultValue),
+    left: mergeSide(operation, left, margin2.left, defaultValue),
+    bottom: mergeSide(operation, bottom, margin2.bottom, defaultValue),
+    right: mergeSide(operation, right, margin2.right, defaultValue),
+  };
+}
+
+export const mergePadding = mergeMarginOrPadding;
+export const mergeMargin = mergeMarginOrPadding;
